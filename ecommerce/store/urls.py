16d253acf61499cfd248b2c24833c9521f7f29b2
IlyaNesterov <Eildos95@gmail.com>
2021-06-15 21:29:15 +0300

    Create urls

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.store, name='store'),
-    path('cart/', views.store, name='cart'),
-    path('checkout', views.store, name='checkout'),
+    path('', views.store, name="store"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout"),
 ]
