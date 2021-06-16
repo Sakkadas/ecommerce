436d5d575a1b0b30f78d828e0ee205b4286f5b68
IlyaNesterov <Eildos95@gmail.com>
2021-06-17 00:08:15 +0300

    add JavaScript event handlers

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+
 from .models import *
 
 
@@ -30,3 +32,7 @@ def checkout(request):
         order = {'get_cart_total': 0, 'get_cart_items': 0}
     context = {"items": items, 'order': order}
     return render(request, 'store/checkout.html', context)
+
+
+def updateItem(request):
+    return JsonResponse('Item was added', safe=False)
