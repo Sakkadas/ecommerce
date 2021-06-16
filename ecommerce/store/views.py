429d1c9439c399e05da9f52326163ec5530864b2
IlyaNesterov <Eildos95@gmail.com>
2021-06-16 23:14:51 +0300

    make checkout view dynamic

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -21,5 +21,12 @@ def cart(request):
 
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+    context = {"items": items, 'order': order}
     return render(request, 'store/checkout.html', context)
