d3bde09a0f03664e470bb083544c2c6d42ea2acf
IlyaNesterov <Eildos95@gmail.com>
2021-06-16 21:35:39 +0300

    mend imageError (absence pict)

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -20,7 +20,13 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
     date_ordered = models.DateTimeField(auto_now_add=True)
