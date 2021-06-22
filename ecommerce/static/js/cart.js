ca71835e6ac28038233957b029af8f05069631a7
IlyaNesterov <Eildos95@gmail.com>
2021-06-22 18:10:04 +0300

    add addCookieItem for unauthenticated users

--- ecommerce/static/js/cart.js
+++ ecommerce/static/js/cart.js
@@ -7,11 +7,36 @@ for (i = 0; i < updateBtns.length; i++) {
         console.log("productId:", productId, "Action:", action);
         console.log("USER:", user);
         if (user == "AnonymousUser") {
-            console.log('Not authenticated')
+            addCookieItem()
         } else {
             updateUserOrder(productId, action);
         }
     });
+}
+function addCookieItem(productId, action){
+	console.log('User is not authenticated')
+
+	if (action == 'add'){
+		if (cart[productId] == undefined){
+		cart[productId] = {'quantity':1}
+
+		}else{
+			cart[productId]['quantity'] += 1
+		}
+	}
+
+	if (action == 'remove'){
+		cart[productId]['quantity'] -= 1
+
+		if (cart[productId]['quantity'] <= 0){
+			console.log('Item should be deleted')
+			delete cart[productId];
+		}
+	}
+	console.log('CART:', cart)
+	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
+
+	location.reload()
 }
 
 function updateUserOrder(productId, action) {
