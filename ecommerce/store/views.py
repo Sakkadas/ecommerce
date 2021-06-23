71841f055bb34a62b43300aadd2fe4abd5f1fef4
IlyaNesterov <Eildos95@gmail.com>
2021-06-23 16:02:15 +0300

    change structure of processOrder view

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 import json
 import datetime
 
-from .utils import cartData
+from .utils import cartData, cookieCart, guestOrder
 from .models import *
 
 
@@ -73,23 +73,25 @@ def processOrder(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, create = Order.objects.get_or_create(customer=customer, complete=False)
-        total = float(data['form']['total'])
-        order.transaction_id = transaction_id
-
-        if total == order.get_cart_total:
-            order.complete = True
-        order.save()
-
-        if order.shipping == True:
-            shippingAddress.objects.create(
-                customer=customer,
-                order=order,
-                adress=data['shipping']['adress'],
-                city=data['shipping']['city'],
-                state=data['shipping']['state'],
-                zipcode=data['shipping']['zipcode'],
-
-            )
     else:
-        print('User is not logged in..')
+        customer, order = guestOrder(request, data)
+
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
+
+    if total == order.get_cart_total:
+        order.complete = True
+    order.save()
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer=customer,
+            order=order,
+            address=data['shipping']['address'],
+            city=data['shipping']['city'],
+            state=data['shipping']['state'],
+            zipcode=data['shipping']['zipcode'],
+
+        )
+
     return JsonResponse('Payment complete', safe=False)
